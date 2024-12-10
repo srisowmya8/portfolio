e6069c68a26e53cc6c8db96f7aa45f93f72ca201
Srineha_mukku <mukkusrineha.com>
2024-12-10 08:39:49 +0530

    content modified

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -9,6 +9,7 @@ import {
   DiPython,
   DiGit,
   DiJava,
+  DiHtml5,
 } from "react-icons/di";
 // import {
 //   SiRedis,
@@ -17,20 +18,20 @@ import {
 //   SiSolidity,
 //   SiPostgresql,
 // } from "react-icons/si";
-import { TbBrandGolang } from "react-icons/tb";
+//import { TbBrandGolang } from "react-icons/tb";
 
 function Techstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <CgCPlusPlus />
-      </Col> */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiHtml5 />
+      </Col>
       <Col xs={4} md={2} className="tech-icons">
         <DiJavascript1 />
       </Col>
-      <Col xs={4} md={2} className="tech-icons">
+      {/* <Col xs={4} md={2} className="tech-icons">
         <TbBrandGolang />
-      </Col>
+      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiNodejs />
       </Col>
@@ -43,9 +44,6 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiMongodb />
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiNextdotjs />
-      </Col> */}
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
       </Col>
--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -3,26 +3,27 @@ import { Col, Row } from "react-bootstrap";
 import {
   SiVisualstudiocode,
   SiPostman,
-  // SiSlack,
   SiVercel,
-  // SiMacos,
 } from "react-icons/si";
-
+import {
+  DiEclipse,
+  DiWindows,
+} from "react-icons/di";
 function Toolstack() {
   return (
     <Row style={{ justifyContent: "center", paddingBottom: "50px" }}>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiMacos />
-      </Col> */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiWindows />
+      </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVisualstudiocode />
       </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiPostman />
       </Col>
-      {/* <Col xs={4} md={2} className="tech-icons">
-        <SiSlack />
-      </Col> */}
+      <Col xs={4} md={2} className="tech-icons">
+        <DiEclipse />
+      </Col>
       <Col xs={4} md={2} className="tech-icons">
         <SiVercel />
       </Col>
