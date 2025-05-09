8c2e55a74288ab0b8ef55e8edac18f5347634607
srisowmya8 <srisowmya812@gmail.com>
2025-05-09 20:32:38 +0530

    Added name and modified the repo

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -21,7 +21,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> NEHA MUKKU</strong>
+                <strong className="main-name"> Sri Sowmya Indlamudi</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
